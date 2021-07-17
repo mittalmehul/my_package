09f0cb678f62b9bfbeef82aceec9350477b05489
mehul-mittal-credgenics <mehul.mittal@credgenics.com>
2021-07-17 20:40:47 +0530

    update version no

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='my_package_example',
     packages=['my_package'],
     description='My Package Example',
-    version='0.1',
+    version='0.1.1',
     url='',
     author='Mehul',
     author_email='mehul.mittal@credgenics.com',
